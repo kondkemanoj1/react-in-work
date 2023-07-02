bad29ee3f10f8ef28e8b4a6207595817d346a6c7
Manoj Kondke <kondke.manoj1@gmail.com>
2023-07-02 19:35:09 +0530

    First Functional component

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = <h1>Hello React</h1>;
+
+const HeaderComponent = (props) => {
+  const { title } = props;
+  return (
+    <>
+      {heading}
+      <h1> This is Header Component</h1>
+      <h2>Welcome to React - {title}</h2>
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(HeaderComponent({ title: "Manoj" }));
