8c020212da85a74ed0f66bdd9fca1d8e20ec504f
Manoj Kondke <kondke.manoj1@gmail.com>
2023-07-06 00:48:05 +0530

    Added header and restaurant components

--- app.js
+++ app.js
@@ -1,18 +1,94 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1>Hello React</h1>;
+const heading = <h1>Food Villa - By Manoj</h1>;
+const foodData = [
+  {
+    name: "Pizza Bites",
+    slaString: "32 MINS",
+    costForTwoString: "₹300 FOR TWO",
+    cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+    cloudinaryImageId: "be87ef87d60b0e6baa2a24c080065c30",
+  },
+  {
+    name: "Burger King",
+    slaString: "22 MINS",
+    costForTwoString: "₹350 FOR TWO",
+    cuisines: ["Burgers", "American"],
+    cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+  },
+  {
+    name: "Pizza Hut",
+    slaString: "38 MINS",
+    costForTwoString: "₹600 FOR TWO",
+    cuisines: ["Pizzas"],
+    cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+  },
+  {
+    name: "Tea Post",
+    slaString: "52 MINS",
+    costForTwoString: "₹400 FOR TWO",
+    cuisines: ["Beverages", "Fast Food", "Snacks"],
+    cloudinaryImageId: "47a5a4f1020f6715556094680749bada",
+  },
+];
 
 const HeaderComponent = (props) => {
   const { title } = props;
   return (
-    <>
-      {heading}
-      <h1> This is Header Component</h1>
-      <h2>Welcome to React - {title}</h2>
-    </>
+    <div className="header">
+      <img
+        height="100"
+        width="100"
+        src="https://cdn.octopix.in/uploads/company-logo/2020/11/19/food-villa-pSJVhwoN8KxgwV9jtuB1MlosJ0ejoKfiBiVO1jJPLM61shyarbxVvjIFy3DVpbUML8eBxcUo7BOWXQcd-350x350.jpg"
+      />
+      <span> {heading}</span>
+      <ul className="headerMenu">
+        <li> Home</li>
+        <li>Contact Us</li>
+        <li>About Us</li>
+      </ul>
+    </div>
   );
 };
 
+const CardComponent = (props) => {
+  const { name, slaString, costForTwoString, cuisines, cloudinaryImageId } =
+    props;
+  return (
+    <div class="card">
+      <p>
+        <img
+          height="160"
+          width="254"
+          src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`}
+          alt="Restaurant Name"
+        ></img>
+      </p>
+      <div>
+        <strong>{name}</strong>
+        <br></br>
+        <em>{cuisines.join(",")}</em>
+      </div>
+      <p class="starContainer">
+        <span>{slaString}</span>
+        <span>{costForTwoString}</span>
+      </p>
+    </div>
+  );
+};
+
+const FoodListComponent = () => {
+  return foodData.map((food) => {
+    return <CardComponent {...food}></CardComponent>;
+  });
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(HeaderComponent({ title: "Manoj" }));
+root.render(
+  <>
+    <HeaderComponent />
+    <div className="restaurantContainer">
+      <FoodListComponent />
+    </div>
+  </>
+);
