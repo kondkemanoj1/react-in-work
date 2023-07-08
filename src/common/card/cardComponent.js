d669ae92e12c4624951d07bf6718e58c14a598eb
Manoj Kondke <kondke.manoj1@gmail.com>
2023-07-08 21:10:07 +0530

    Styling for cards with grid

--- src/common/card/cardComponent.js
+++ src/common/card/cardComponent.js
@@ -7,8 +7,6 @@ export const CardComponent = (props) => {
     <div className="card">
       <p>
         <img
-          height="160"
-          width="254"
           src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`}
           alt="Restaurant Name"
         ></img>
